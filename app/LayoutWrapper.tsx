9866861cd8cf4d3b9eb3d66e357b835b4bd8b833
mostafa <mostafa.mz.code@gmail.com>
2026-05-20 20:20:07 +0430

    add click-spark animation

--- app/LayoutWrapper.tsx
+++ app/LayoutWrapper.tsx
@@ -1,7 +1,9 @@
 "use client";
 
+import ClickSpark from "@/components/ClickSpark";
 import { usePathname } from "next/navigation";
 import { ReactNode } from "react";
+import { useNotFound } from "./context/NotFoundContext";
 
 interface Props {
   children: ReactNode;
@@ -9,35 +11,38 @@ interface Props {
 
 export default function LayoutWrapper({ children }: Props) {
   const pathname = usePathname();
+  const { isNotFound } = useNotFound(); // Destructure state
 
-  // const backgroundClass = (() => {
-  //   if (pathname === "/")
-  //     return "dark:bg-[url('/bg-home-dark-mobile.jpg')] md:bg-[url('/bg-home-light.webp')] md:dark:bg-[url('/bg-home-dark.webp')]";
-
-  //   if (pathname.startsWith("/about"))
-  //     return "dark:bg-[url('/bg-home-dark-mobile.jpg')] md:bg-[url('/bg-home-light.webp')] md:dark:bg-[url('/bg-home-dark.webp')]";
-
-  //   if (pathname.startsWith("/projects"))
-  //     return "dark:bg-[url('/bg-home-dark-mobile.jpg')] md:bg-[url('/bg-home-light.webp')] md:dark:bg-[url('/bg-home-dark.webp')]";
-
-  //   if (pathname.startsWith("/contact"))
-  //     return "dark:bg-[url('/bg-home-dark-mobile.jpg')] md:bg-[url('/bg-home-light.webp')] md:dark:bg-[url('/bg-home-dark.webp')]";
-
-  //   return "bg-[url('/bg-default.webp')]";
-  // })();
+  // Set your backgrounds dynamically based on page or 404 state
+  const backgroundClass = (() => {
+    if (isNotFound) {
+      return "bg-[url('/bg-404.webp')]"; // Your 404 background
+    }
+    return "bg-[url('/bg-home-dark-mobile.jpg')] md:bg-[url('/bg-home-light.webp')] md:dark:bg-[url('/bg-home-dark.webp')]";
+  })();
 
   const isProjectsOrContactPage =
     pathname.startsWith("/projects") || pathname.startsWith("/contact");
 
   return (
     <div
-      className={`relative mx-auto bg-contain md:bg-cover bg-repeat-y ${"bg-[url('/bg-home-dark-mobile.jpg')] md:bg-[url('/bg-home-light.webp')] md:dark:bg-[url('/bg-home-dark.webp')]"}`}
+      className={`relative mx-auto bg-contain md:bg-cover bg-repeat-y ${backgroundClass}`}
     >
-      {isProjectsOrContactPage && (
+      {isProjectsOrContactPage && !isNotFound && (
         <div className="absolute inset-0 bg-black/45 pointer-events-none" />
       )}
 
-      <div className="relative z-10">{children}</div>
+      <div className="relative z-10">
+        <ClickSpark
+          sparkColor="#10B981"
+          sparkSize={10}
+          sparkRadius={20}
+          sparkCount={8}
+          duration={400}
+        >
+          {children}
+        </ClickSpark>
+      </div>
     </div>
   );
 }
